0add4164f65695dc1a21aa28e30c5dc9f3e4c291
Aabbsiroc <1234567kazakova@gmail.com>
2024-10-25 14:52:26 +0400

    commit 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+package main.java;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 1; i <= 100; i++) {
+            ans.append(i);
+            if (i % 10 == 0) {
+                ans.append("\n");
+            } else {
+                ans.append(", ");
+            }
+        }
+        System.out.println(ans.toString());
+    }
+}
